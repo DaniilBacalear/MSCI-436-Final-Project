86dbffb6eb1ad971e278fc428276040b1723bde3
Daniil Bacalear <daniil.bacalear@uwaterloo.ca>
2022-07-14 19:58:21 -0400

    added a pie chart

--- streamlit-app.py
+++ streamlit-app.py
@@ -1,10 +1,39 @@
-import streamlit as st
 from module import evaluate_csv
 import pandas as pd
-uploaded_file = st.file_uploader('upload csv', type=['csv'],accept_multiple_files=False,key="fileUploader")
+import streamlit as st
+import matplotlib.pyplot as plt
+
+
+def get_prediction(data_set):
+    dataframe = pd.read_csv(data_set)
+    print('loading model')
+    prediction = evaluate_csv(dataframe)
+    return prediction
+
+
+def display_prediction(prediction):
+    st.dataframe(prediction[["amount", "nameOrig", "oldbalanceOrg", "newbalanceOrig", "nameDest", "oldbalanceDest",
+                             "newbalanceDest", "isFraud", "predictedIsFraud"]])
+
+
+def make_pi_chart(prediction):
+    fraud_percentage = (prediction['predictedIsFraud'].sum() / len(prediction['predictedIsFraud'])) * 100
+    valid_percentage = 100 - fraud_percentage
+    labels = 'Fraudulent', 'Valid'
+    colors = ['#E52C04', '#1EEC06']
+    sizes = [fraud_percentage, valid_percentage]
+    explode = (0.1, 0.0)  # only "explode" the 2nd slice (i.e. 'Hogs')
+    fig1, ax1 = plt.subplots()
+    ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+            colors=colors, shadow=True, startangle=90)
+    ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+    st.pyplot(fig1)
+
+
+uploaded_file = st.file_uploader('upload csv', type=['csv'], accept_multiple_files=False, key="fileUploader")
 
 if uploaded_file is not None:
-    dataframe = pd.read_csv(uploaded_file)
-    print('loading model')
-    df = evaluate_csv(dataframe)
-    st.dataframe(df[["amount", "nameOrig", "oldbalanceOrg", "newbalanceOrig", "nameDest", "oldbalanceDest", "newbalanceDest", "isFraud", "predictedIsFraud"]])
+    prediction = get_prediction(uploaded_file)
+    display_prediction(prediction)
+    make_pi_chart(prediction)
+
